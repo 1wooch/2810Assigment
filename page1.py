107cd078548b71c0f73e8600190de19381b04710
Wonwoo Choi <onechoo1106@gmail.com>
2022-09-25 22:24:34 +1000

    finish search by date

--- page1.py
+++ page1.py
@@ -1,24 +1,39 @@
+from datetime import datetime
+from tracemalloc import start
 import pandas
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+def q1(start_date,end_date):
+    test=pandas.read_csv('data.csv')
+    #get a data from data.csv
+    test['OFFENCE_MONTH']=pandas.to_datetime(test['OFFENCE_MONTH'])
+    #convert OFFENCE_MONTH into date format.
+    start_date=datetime.strptime(start_date, '%m-%d-%Y').date()
+    #convert start date into date format.
+    count=0
+    count1=0
+    print(type(test.loc[0]['OFFENCE_MONTH']))
 
-test=pandas.read_csv('data.csv')
-test['OFFENCE_MONTH']=pandas.to_datetime(test['OFFENCE_MONTH'])
+    #print(len(test.index)) #length of csv file?
+    for x in test.index:
+        if test.loc[x,"OFFENCE_MONTH"]>pandas.Timestamp(start_date):
+            count+=1
+    for x in test.index:
+        if test.loc[x,"OFFENCE_MONTH"]>pandas.Timestamp(end_date):
+            count1+=1
 
-count=0
+    print(count)
+    print(count1)
+    xpoints = np.array([0,6])
+    ypoints = np.array([0,count])
+    plt.plot(xpoints,ypoints)
+    plt.show()
 
-print(len(test.index)) #length of csv file?
-for x in test.index:
-    if test.loc[x,"FACE_VALUE"]>300:
-        count+=1
+    #print(test.loc[0][1])
+    #print(type(test.loc[0][1]))
+    print(count)
 
-xpoints = np.array([0,6])
-ypoints = np.array([0,count])
-plt.plot(xpoints,ypoints)
-plt.show()
+q1('01-01-2012','01-01-2013')
 
-#print(test.loc[0][1])
-#print(type(test.loc[0][1]))
-print(count)
 
