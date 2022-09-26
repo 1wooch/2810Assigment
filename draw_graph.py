2e901584bf2d5b67200d72d0c998f57593ba8e51
Wonwoo Choi <onechoo1106@gmail.com>
2022-09-27 02:58:00 +1000

    add sort dictionary

--- draw_graph.py
+++ draw_graph.py
@@ -14,9 +14,10 @@ class draw_graph(object):
     def __init__(self,month_result):
         self.month_result=month_result
         
+
         
     
-    def draw_bar_graph(self):
+    def draw_bar_graph(self): #mobile phone , School Zone
         plt.figure(figsize=(20,3))
         #width:20,height:3
         plt.title('Bar Graph')
@@ -28,18 +29,21 @@ class draw_graph(object):
         plt.title('Line Graph')
         #print(self.month_result.keys())
         #print(self.month_result.values())
-        x=np.array(list(self.month_result.keys())) #month
+        labels=np.array(list(self.month_result.keys())) #month
         y=np.array(list(self.month_result.values())) #data
-        plt.plot(x,y,linestyle='-',marker='o')
+        plt.plot(labels,y,linestyle='-',marker='o')
         plt.show()
-    def draw_2_line_graph(self):
-        date_list=np.array(list(self.month_result.keys()))
+    def draw_2_line_graph(self): #for radar or camera
+        labels=np.array(list(self.month_result.keys()))
         first_data=np.array(list(self.month_result.values())).astype(np.double)
         second_data=np.array([2, None, 5, None, 4, None, 3, 2]).astype(np.double)
-        plt.plot(date_list,first_data,linestyle='-',marker='o')
-        plt.plot(date_list,first_data,linestyle='-',marker='o')
+        plt.plot(labels,first_data,linestyle='-',marker='o')
+        plt.plot(labels,first_data,linestyle='-',marker='o')
         plt.show()
-
+    def draw_pie_chart(self,test_value):
+        data=np.array(list(self.month_result.values()))
+        #print(data)
+        labels=np.array(list(self.month_result.keys()))
 
 
 
